041764be99513ccc56b9e70830a2d7af0e93917c
James Elford <james.elford08@imperial.ac.uk>
2011-07-11 01:04:37 +0100

    Added filename processing to produce unique names for output

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 #include <cairo/cairo.h>
+#include <sys/stat.h>
+#include <string>
+#include <string.h>
+#include <sstream>
+#include <time.h>
 #include "keyboard.hpp"
 #include "screenshot.hpp"
+
+std::string output_dir = "output";
+
+void check_output_directory() {
+	// Don't actually care if this fails, so long as we can write there...
+	int n = mkdir(output_dir.c_str(), 0777);
+}
+
+std::string get_output_filename() {
+	time_t epoch_time;
+	epoch_time = time(NULL);
+	std::stringstream output;
+	output <<  output_dir;
+	output << "/screengrab_";
+	output << epoch_time;
+	output << ".png";
+	return output.str();
+}
 
 class ScreenGrabHandler {
 	public:
@@ -9,7 +32,7 @@ class ScreenGrabHandler {
 
 		bool operator()(void) {
 			cairo_surface_t* screen_image = sg.grab_screen();
-			cairo_surface_write_to_png(screen_image, "out.png");
+			cairo_surface_write_to_png(screen_image, get_output_filename().c_str());
 			cairo_surface_destroy(screen_image);
 			return true;
 		}
@@ -25,7 +48,7 @@ class QuitHandler {
 };
 
 int main(int argc, char ** argv) {
-	
+	check_output_directory();
 	KeyboardGrabber keyboard;
 	ScreenGrabber screenGrabber;
 	ScreenGrabHandler screenHandler(screenGrabber);
